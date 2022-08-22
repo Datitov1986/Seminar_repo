62abbaf9fa3ccb428bf370d8519c8655f01067fd
Dmitrii <datitov2009@yandex.ru>
2022-08-22 22:03:23 +0300

    added seminar 22.08.22 comment

--- Seminar22.08.22/Program.cs
+++ Seminar22.08.22/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System.Security.Cryptography;
 using System;
-Random rand = New Random();
+Random rand = new Random();
 //Создаем переменную для рандома
 int num = rand.Next(10,100);
 int a1 = num / 10;
 int a2 = num % 10;
-itn max = a1;
+int max = a1;
 if (max < a2)
 {
     max = a2;
 }
 Console.WriteLine(max);
+//Оператор "И" &&
+//Оператор "ИЛИ" ||
+//Оператор "Не" !
