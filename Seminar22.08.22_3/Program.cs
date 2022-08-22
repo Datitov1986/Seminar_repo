a80ee36bf0cf4848fe97173bb71285895a5fe782
Dmitrii <datitov2009@yandex.ru>
2022-08-22 21:27:29 +0300

    added seminar 22.08.22_3

---
+++ Seminar22.08.22_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Dynamic;
+using System;
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = b % a;
+if(result == 0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else
+{
+    Console.WriteLine("Не кратно, остаток: " + result);
+}
