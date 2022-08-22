8b578c522ab541fb6b0ca93a1c5b0bcf97b2f50a
Dmitrii <datitov2009@yandex.ru>
2022-08-22 20:53:33 +0300

    added seminar 22.08.22 folder

---
+++ Seminar22.08.22/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using System;
+Random rand = New Random();
+//Создаем переменную для рандома
+int num = rand.Next(10,100);
+int a1 = num / 10;
+int a2 = num % 10;
+itn max = a1;
+if (max < a2)
+{
+    max = a2;
+}
+Console.WriteLine(max);
