95a42d3a586a4e2180a32f189bfa6e4d3b481607
Dmitrii <datitov2009@yandex.ru>
2022-08-22 22:41:18 +0300

    Debugged

--- Seminar22.08.22_2/Program.cs
+++ Seminar22.08.22_2/Program.cs
@@ -7,10 +7,10 @@
 using System;
 Random rnd = new Random();
 int num = rnd.Next(100,1000);
-Console.WriteLine(num);
+Console.WriteLine("Рандомное число: " + num);
 int x = num / 100;
 x = x*10;
-x = x + (x%10);
+x = x + (num%10);
 int result = (num / 100) * 10 + num % 10;
 Console.WriteLine($"its -> {x}");
 
