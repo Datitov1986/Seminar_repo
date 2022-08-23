c38dca4392c889b4593798808097640c445d3457
Dmitri <datitov2009@yandex.ru>
2022-08-23 18:27:03 +0300

    Навел немного лоска

--- Seminar22.08.22_4/Program.cs
+++ Seminar22.08.22_4/Program.cs
@@ -9,9 +9,9 @@ Console.Write("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 if(a % 7 == 0 && a % 23 == 0)
 {
-    Console.WriteLine("Yes");
+    Console.WriteLine("Кратно");
 }
 else
 {
-    Console.WriteLine("No");
+    Console.WriteLine("Не кратно");
 }
