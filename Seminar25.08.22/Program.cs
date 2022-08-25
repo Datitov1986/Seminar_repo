bff5507f0cc30acc2facf165167d9f13b67108de
Dmitri <datitov2009@yandex.ru>
2022-08-25 20:48:34 +0300

    add Seminar 25.08.22 folder

---
+++ Seminar25.08.22/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Точка находится в " + ChetvertVPloskosti(x,y) + " четверти");
+
+int ChetvertVPloskosti(int X, int Y)
+{
+    if(X > 0 && Y > 0)
+        return 1;
+
+    if(X < 0 && Y > 0)
+        return 2;
+    
+    if(X < 0 && Y < 0)
+        return 3;
+
+    if(X > 0 && Y < 0)
+        return 4;
+    
+    return 0;
+}
