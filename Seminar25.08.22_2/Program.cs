a3de76d994aba8369d1c99e9c35572951eea75cf
Dmitri <datitov2009@yandex.ru>
2022-08-26 08:03:05 +0300

    add Seminar 25.08.22_2 folder

---
+++ Seminar25.08.22_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Console.WriteLine("В какой четверти находится ваша точка?");
+// int chetv = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Диапазон точек " + point(chetv));
+
+// string point(int chetv)
+// {
+//     if(chetv == 1)
+//         return "+et; +et";
+//     if(chetv == 2)
+//         return "-et; +et";
+//     if(chetv == 3)
+//         return "-et;-et";
+//     if(chetv == 4)
+//         return "+et;-et";
+//     return "Нет такой четверти";
+// }
+
+//Вариант решения с функцией void
+
+Console.WriteLine("В какой четверти находится ваша точка?");
+int chetv = Convert.ToInt32(Console.ReadLine());
+point(chetv);
+
+int point(int chetv);
+{
+    if(chetv == 1)
+        Console.WriteLine("+et;+et");
+    if(chetv == 2)
+        Console.WriteLine("-et;+et");
+    if(chetv == 3)
+        Console.WriteLine("-et;-et");
+    if(chetv == 4)
+        Console.WriteLine("+et;-et");
+}
