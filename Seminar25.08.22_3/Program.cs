579107bf7a67e2a2450854f4618cf24388152e5b
Dmitrii <datitov2009@yandex.ru>
2022-08-26 22:34:53 +0300

    add Task 3 square table

--- Seminar25.08.22_3/Program.cs
+++ Seminar25.08.22_3/Program.cs
@@ -4,29 +4,55 @@
 двух точек и находит расстояние между ними в 2D пространстве.
 A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21
 */
-using System;
+// using System;
 
-void FindDistance(int X1, int Y1, int X2, int Y2)
-{
-    int DeltaX = (X2 - X1);
-    int DeltaY = (Y2 - Y1);
-    int stepen = 2;
-    int x = Convert.ToInt32(Math.Pow(DeltaX, stepen));
-    int y = Convert.ToInt32(Math.Pow(DeltaY, stepen));
-    double result = Math.Sqrt (x + y);
-    Console.WriteLine("Дистанция между точками = " + result);
-}
-Console.Write("ВВедите координату X1: ");
-int X1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("ВВедите координату Y1: ");
-int Y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("ВВедите координату X1: ");
-int X2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("ВВедите координату Y1: ");
-int Y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Координаты точки А: (" + X1 + ";" + Y1 + ")");
-Console.WriteLine("Координаты точки B: (" + X2 + ";" + Y2 + ")");
+// void FindDistance(int X1, int Y1, int X2, int Y2)
+// {
+//     int DeltaX = (X2 - X1);
+//     int DeltaY = (Y2 - Y1);
+//     int stepen = 2;
+//     int x = Convert.ToInt32(Math.Pow(DeltaX, stepen));
+//     int y = Convert.ToInt32(Math.Pow(DeltaY, stepen));
+//     double result = Math.Sqrt (x + y);
+//     Console.WriteLine("Дистанция между точками = " + result);
+// }
+// Console.Write("ВВедите координату X1: ");
+// int X1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("ВВедите координату Y1: ");
+// int Y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("ВВедите координату X1: ");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("ВВедите координату Y1: ");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Координаты точки А: (" + X1 + ";" + Y1 + ")");
+// Console.WriteLine("Координаты точки B: (" + X2 + ";" + Y2 + ")");
 
-FindDistance(X1, Y1, X2, Y2);
+// FindDistance(X1, Y1, X2, Y2);
+
+
+// 3 задание 
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+// using System;
+
+// void SquareTable(int N, int count)
+// {
+//     Console.Write("Таблица квадратов от 1 до " + N + ": ");
+//     while(count <= N)
+//     {
+//         int square = count * count;
+//         Console.Write(square + ", ");
+//         count++;
+//     }
+    
+// }
+// Console.Write("Введите ваше число: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// SquareTable(N, count);
+
+
 
 
