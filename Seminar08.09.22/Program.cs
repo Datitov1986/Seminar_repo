b95767b53a4bfb5a6c4cfe73e0a8b9846802c9b5
Dmitri <datitov2009@yandex.ru>
2022-09-12 18:18:59 +0300

    add Seminar 09.09.22 folder

--- Seminar08.09.22/Program.cs
+++ Seminar08.09.22/Program.cs
@@ -169,3 +169,51 @@ m = 3, n = 4.
 // int n = Convert.ToInt32(Console.ReadLine());
 // int[,] array = new int[m, n];
 // FuelArray(m, n, array);
+
+
+
+
+/*Задача 51**: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+Например, задан массив (за один цикл):
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Сумма элементов главной диагонали: 1+9+2 = 12
+*/
+
+void FuelArray(int m, int n, int[,] array)
+{
+	int summ = 0;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			array[i, j] = new Random().Next(1, 10);
+		}
+	}
+	for (int i = 0; i < array.GetLength(0); i++)
+	{
+		for (int j = 0; j < array.GetLength(1); j++)
+		{
+			Console.Write(array[i, j] + " ");
+		}
+		Console.WriteLine();
+	}
+
+	for (int i = 0, j = 0; i < array.GetLength(0) && j < array.GetLength(1); i++, j++)
+	{
+		
+	{
+		if(i == j)summ += array[i,j];
+	}
+		
+	}
+	Console.WriteLine("Сумма равна: " + summ);
+}
+
+Console.WriteLine("Введите колличество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите колличество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+FuelArray(m, n, array);
